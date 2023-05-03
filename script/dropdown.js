a56280c586289abc498e4fef34e06e584f002f74
pedroT <pedrotmartin00@hotmail.com>
2023-05-03 20:30:24 -0300

    menu dropdown feito

--- script/dropdown.js
+++ script/dropdown.js
@@ -1,10 +1,10 @@
-const dropbutton = document.querySelector("#dropdown_button");
-const drop = document.querySelector("dropdown");
+const dropdownBtn = document.querySelector('.dropdown_btn');
+const dropdownMenu = document.querySelector('.dropdown_menu');
 
-dropbutton.addEventListener("click", function(){
-    drop.classList.toggle("show");
-})
+dropdownBtn.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('show');
+});
 
-drop.addEventListener("change", function(){
-    drop.classList.remove("show");
-})
+dropdownMenu.addEventListener('mouseleave', () => {
+  dropdownMenu.classList.remove('show');
+});
