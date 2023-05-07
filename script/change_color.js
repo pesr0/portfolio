33a6d63dc6cdcf2b6087f0ab9e181502ee5abd3c
pedroT <pedrotmartin00@hotmail.com>
2023-05-07 20:57:10 -0300

    correção de bug transicao de cor abrupta

--- script/change_color.js
+++ script/change_color.js
@@ -3,10 +3,13 @@ const colors = ['red', 'blue', 'green', 'pink', 'yellow', 'purple', 'nude']; // 
 let colorIndex = 0; // Variável para rastrear o índice atual da cor
 
 function changeColor() {
-    textElement[0].style.color=colors[colorIndex];
-    textElement[1].style.color=colors[colorIndex+1];
-    colorIndex = (colorIndex + 1) % colors.length; // Altera o índice da cor para a próxima cor no array
-    setTimeout(changeColor, 1000);
+    setTimeout(() => {
+        textElement[0].style.color=colors[colorIndex];
+        textElement[1].style.color=colors[colorIndex+1];
+        colorIndex = (colorIndex + 1) % colors.length; // Altera o índice da cor para a próxima cor no array
+        changeColor();
+
+    }, 1000);
   };
   
   changeColor();
