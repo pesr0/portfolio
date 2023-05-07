936467d281a24acdc65ffd91856dc362e46c8123
pedroT <pedrotmartin00@hotmail.com>
2023-05-07 20:27:46 -0300

    arrumada toggle de paginas, inseridos mais projetos

--- script/toggle_page.js
+++ script/toggle_page.js
@@ -4,21 +4,29 @@ const landingLnk = document.querySelector('#title_btn');
 const aboutmePg = document.querySelector('#aboutme_page');
 const projectsPg = document.querySelector('#projects_page');
 const landingPg = document.querySelector('#landing_page');
+const aboutmeHL = document.querySelector('#aboutme_hyperlink');
+const projectsHL = document.querySelector('#projects_hyperlink');
 
-aboutmeBtn.addEventListener('click', () => {
+
+function aboutme_block() {
   aboutmePg.style.display='block';
   projectsPg.style.display='none';
   landingPg.style.display='none';
-});
+};
 
-projectsBtn.addEventListener('click', () => {
+function projects_block() {
   aboutmePg.style.display='none';
   projectsPg.style.display='block';
   landingPg.style.display='none';
-});
+};
 
 landingLnk.addEventListener('click', () => {
   aboutmePg.style.display='none';
   projectsPg.style.display='none';
   landingPg.style.display='block';
 });
+
+aboutmeBtn.addEventListener('click', aboutme_block);
+projectsBtn.addEventListener('click', projects_block);
+aboutmeHL.addEventListener('click', aboutme_block);
+projectsHL.addEventListener('click', projects_block);
